19d1080ffbb9844032803131df3eb2879942851a
Ahmed Abdelazim <toshiba20.ahmed@gmail.com>
2019-07-06 21:10:06 +0000

    fixed sec-input.sh bugs

--- sec-input.sh
+++ sec-input.sh
@@ -12,20 +12,18 @@ confirm() {
             ;;
     esac
 }
-#rm report.txt
+rm report.txt
 #jq -r '.[0]."aud-cmd"'  input.json ;
-#len=$(jq '. | length' input.json) ;
-len=100
+len=$(jq '. | length' input.json) ;
+#len=1
 x=0
-while [[ $x -lt $len ]]
+while [[ $x -le $len ]]
 do
 echo $(jq --arg x $x -r '[.[] ] | .[$x | tonumber]."desc"' input.json) >> report.txt;
 echo $(jq --arg x $x -r '[.[] ] | .[$x | tonumber]."aud-cmd"' input.json) > temp.sh && bash temp.sh >> report.txt && rm temp.sh ;
-echo $(jq --arg x $x -r '[.[] ] | .[$x | tonumber]."aud-desc"' input.json) >> report.txt;
+echo $(jq --arg x $x -r '[.[] ] | .[$x | tonumber]."aud-desc"' input.json) >> report.txt ;
 if [[ `jq --arg x $x -r '[.[] ] | .[$x | tonumber]."rem-exec"' input.json` -eq 1 ]] ; then echo $(jq --arg x $x -r '[.[] ] | .[$x | tonumber]."rem-cmd"' input.json) > temp.sh && bash temp.sh >> report.txt && rm temp.sh ; fi
-if [[ `jq --arg x $x -r '[.[] ] | .[$x | tonumber]."rem-exec"' input.json` -eq 1 ]] ; then jq --arg x $x -r '[.[] ] | .[$x | tonumber]."rem-desc"' input.json >> report.txt;
-#jq --arg x $x -r '[.[] ] | .[$x | tonumber]."desc"' input.json ;
+if [[ `jq --arg x $x -r '[.[] ] | .[$x | tonumber]."rem-exec"' input.json` -eq 1 ]] ; then echo $(jq --arg x $x -r '[.[] ] | .[$x | tonumber]."rem-desc"' input.json) >> report.txt ; fi
 echo $x >> report.txt ;
 x=$(( $x + 1 )) ;
-echo $x ;
 done
