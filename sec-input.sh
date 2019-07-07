bdedc5999b4b0ea0438e3ba13deeb37a6c63bc55
Ahmed Abdelazim <toshiba20.ahmed@gmail.com>
2019-07-07 20:37:44 +0000

    used html instead

--- sec-input.sh
+++ sec-input.sh
@@ -12,18 +12,49 @@ confirm() {
             ;;
     esac
 }
-rm report.txt
+rm report.html
+echo "<!DOCTYPE html>
+<html>
+<title>Report</title>
+
+<xmp theme=\"united\" style=\"display:none;\">" > report.html
 #jq -r '.[0]."aud-cmd"'  input.json ;
 len=$(jq '. | length' input.json) ;
 #len=1
 x=0
-while [[ $x -le $len ]]
+while [[ $x -lt $len ]]
 do
-echo $(jq --arg x $x -r '[.[] ] | .[$x | tonumber]."desc"' input.json) >> report.txt;
-echo $(jq --arg x $x -r '[.[] ] | .[$x | tonumber]."aud-cmd"' input.json) > temp.sh && bash temp.sh >> report.txt && rm temp.sh ;
-echo $(jq --arg x $x -r '[.[] ] | .[$x | tonumber]."aud-desc"' input.json) >> report.txt ;
-if [[ `jq --arg x $x -r '[.[] ] | .[$x | tonumber]."rem-exec"' input.json` -eq 1 ]] ; then echo $(jq --arg x $x -r '[.[] ] | .[$x | tonumber]."rem-cmd"' input.json) > temp.sh && bash temp.sh >> report.txt && rm temp.sh ; fi
-if [[ `jq --arg x $x -r '[.[] ] | .[$x | tonumber]."rem-exec"' input.json` -eq 1 ]] ; then echo $(jq --arg x $x -r '[.[] ] | .[$x | tonumber]."rem-desc"' input.json) >> report.txt ; fi
-echo $x >> report.txt ;
+echo "## $(jq --arg x $x -r '[.[] ] | .[$x | tonumber]."desc"' input.json)" >> report.html;
+echo "### Used command" >> report.html;
+if [[ $(jq --arg x $x -r '[.[] ] | .[$x | tonumber]."aud-cmd"' input.json) != "" ]] ; then echo "\`\`\`" >> report.html; fi
+if [[ $(jq --arg x $x -r '[.[] ] | .[$x | tonumber]."aud-cmd"' input.json) != "" ]] ; then echo $(jq --arg x $x -r '[.[] ] | .[$x | tonumber]."aud-cmd"' input.json) >> report.html; else echo "No command" >> report.html; fi
+if [[ $(jq --arg x $x -r '[.[] ] | .[$x | tonumber]."aud-cmd"' input.json) != "" ]] ; then echo "\`\`\`" >> report.html; fi
+echo "
+### Result
+" >> report.html;
+echo "\`\`\`" >> report.html;
+if [[ $(jq --arg x $x -r '[.[] ] | .[$x | tonumber]."aud-cmd"' input.json) != "" ]] ;
+then echo $(jq --arg x $x -r '[.[] ] | .[$x | tonumber]."aud-cmd"' input.json) > temp.sh && bash temp.sh >> report.html && rm temp.sh ;
+else echo "NO output from command"  >> report.html
+fi
+echo "
+" >> report.html;
+echo "\`\`\`" >> report.html;
+echo "<br>" >> report.html;
+if [[ $(jq --arg x $x -r '[.[] ] | .[$x | tonumber]."aud-desc"' input.json) != "" ]] ; then echo "<p> $(jq --arg x $x -r '[.[] ] | .[$x | tonumber]."aud-desc"' input.json) </p>" >> report.html; fi
+#echo "\`\`\`" >> report.html;
+if [[ `jq --arg x $x -r '[.[] ] | .[$x | tonumber]."rem-exec"' input.json` -eq 1 ]] ; then echo $(jq --arg x $x -r '[.[] ] | .[$x | tonumber]."rem-cmd"' input.json) > temp.sh && bash temp.sh >> report.html && rm temp.sh ; fi
+#echo "\`\`\`" >> report.html;
+if [[ `jq --arg x $x -r '[.[] ] | .[$x | tonumber]."rem-exec"' input.json` -eq 1 ]] ; then echo $(jq --arg x $x -r '[.[] ] | .[$x | tonumber]."rem-desc"' input.json) >> report.html ; fi
+#echo $x >> report.html ;
+echo $x ;
+echo "
+---" >> report.html;
 x=$(( $x + 1 )) ;
 done
+echo "
+</xmp>
+
+<script src=\"http://strapdownjs.com/v/0.2/strapdown.js\"></script>
+</html>" >> report.html
+#markdown report.html -f fencedcode > md.html
